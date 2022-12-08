30b272d37283d09ac2a619902fd619d22bd380c4
MihaLis333 <m190333@ya.ru>
2022-12-08 16:34:02 +0300

    не доделанное решение задачи 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+/*
+int firdQuadrant(double x, double y)
+{
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x < 0 && y < 0) return 3;
+    if(x > 0 && y < 0) return 4;
+    return 0;
+}
+
+Console.Write("Ввудите X координату: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y координату: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+
+int quadNum = firdQuadrant(xA, yA);
+
+Console.WriteLine($"Точка A({xA}, {yA}) находиться в квадранте {quadNum}");
+*/
+
+//Задача 1. Напишите программу которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти.
+
+void showQuad(int quad)
+{
+    if(quad == 1) Console.WriteLine("x > 0, y > 0");
+}
